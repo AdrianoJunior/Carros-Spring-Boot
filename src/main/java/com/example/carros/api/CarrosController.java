8cc4ea2706cf0d6d6868d8bc30cf08253b483ef4
AdrianoJunior <adriano.santoscoutinhojr@gmail.com>
2025-06-28 19:30:02 -0300

    Versão inicial da API dos Carros com os métodos basicos (get, put, post, delete)

---
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -0,0 +1,53 @@
+package com.example.carros.api;
+
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/carros")
+public class CarrosController {
+
+    @Autowired
+    private CarroService service;
+
+    @GetMapping
+    public Iterable<Carro> getCarros() {
+        return service.getCarros();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Carro> getById(@PathVariable Long id) {
+        return service.getCarroById(id);
+    }
+
+    @GetMapping("/tipo/{tipo}")
+    public Iterable<Carro> getByTypo(@PathVariable String tipo) {
+        return service.getCarrosByTipo(tipo);
+    }
+
+    @PostMapping
+    public String post(@RequestBody Carro carro) {
+        Carro c = service.save(carro);
+
+        return "Carro salvo com sucesso: " + c.getId();
+    }
+
+    @PutMapping("/{id}")
+    public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+
+        Carro c = service.update(carro, id);
+
+        return "Carro atualizado com sucesso: " + c.getId();
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return "Carro deletado com sucesso: " + id;
+    }
+
+}
---
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -0,0 +1,59 @@
+package com.example.carros.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
+
+
+@Service
+public class CarroService {
+
+    @Autowired
+    private CarroRepository repository;
+
+    public Iterable<Carro> getCarros() {
+        return repository.findAll();
+    }
+
+    public Optional<Carro> getCarroById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Iterable<Carro> getCarrosByTipo(String tipo) {
+
+        return repository.findByTipo(tipo);
+
+    }
+
+    public Carro save(Carro carro) {
+        return repository.save(carro);
+    }
+
+    public Carro update(Carro carro, Long id) {
+        Assert.notNull(id, "não foi possível atualizar o registro");
+
+        // Busca o carro no banco de dados
+        Optional<Carro> optional = getCarroById(id);
+
+        if (optional.isPresent()) {
+            Carro db = optional.get();
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro id " + id + " atualizado com sucesso: " + db.getId());
+
+            repository.save(db);
+            return db;
+        } else {
+            throw new RuntimeException("Não foi possível atualizar o registro");
+        }
+    }
+
+    public void delete(Long id) {
+        Optional<Carro> optional = getCarroById(id);
+        if (optional.isPresent()) {
+            repository.deleteById(id);
+        }
+    }
+}
